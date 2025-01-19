2ea3c813b53d9e5431e84142106e267ec899a638
Nathaniel Chute <nt565821@dal.ca>
2025-01-18 21:23:44 -0400

    Add Proceesbuilder code for running cmd

--- src/App.java
+++ src/App.java
@@ -1,5 +1,18 @@
+import java.io.*;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws IOException {
+        System.out.println(System.getProperty("user.dir"));
+
+        String[] encode
+                = {"opt/homebrew/bin/ffmpeg", "-i", "\"Solo.Leveling.S02E14.mkv\"",
+                "-codec", "copy", "-strict", "-2", "video.mp4"};
+        ProcessBuilder processBuilder = new ProcessBuilder(encode);
+
+        String[] burn
+                = {"ffmpeg", "-i", "video.mp4", "-vf",
+                "\"ass=Solo.Leveling.S02E14.ass\"", "-c:v", "libx264", "-crf", "13", "out.mp4"};
+
+        Process process = processBuilder.start();
     }
 }
